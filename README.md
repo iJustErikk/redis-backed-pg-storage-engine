bb8f32126ba84f65d6f68e05e411d994c442fb6e
Erik Awwad <eawwad@censys.com>
2024-02-11 09:41:02 -0500

    compile; need to resolve issues with TAM not in dylib

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ a fantastically cursed weekend project I need to get to
 inspired by phil eaton: https://notes.eatonphil.com/2024-01-09-minimal-in-memory-storage-engine-for-mysql.html
 
 I'll hack this out the weekend of 2/5/2024
+
+
+## blog steps
+
+- explain storage engines and why this is funny, note this was inspired by phil
+- add stubs
+- notice that attr macro has compile issues => look at why indexamroutine + zombodb works (search for `unsafe impl SqlTranslatable for crate::IndexAmRoutine`) => figure out you need to third party or make a PR => third party (note that I'll make a PR once I understand this better)
+- checkout that third party to `2244e62456390bc10faae99cae3801dc3b05e640`
+- keep breaking this until it works
--- src/lib.rs
+++ src/lib.rs
@@ -47,20 +47,75 @@ pub mod pg_test {
 // redis
 // boom done
 
-// #[pg_extern]
-// fn amhandler(_fcinfo: pg_sys::FunctionCallInfo) -> PgBox<pg_sys::TableAmRoutine> {
-//     let mut amroutine =
-//         unsafe { PgBox::<pg_sys::TableAmRoutine>::alloc_node(pg_sys::NodeTag_T_TableAmRoutine) };
+#[pg_extern(sql = "
+CREATE OR REPLACE FUNCTION redis_tableam_handler(internal)
+RETURNS table_am_handler AS 'redis_backed_storage', 'redis_tableam_handler'
+LANGUAGE C STRICT;
+
+CREATE ACCESS METHOD redis TYPE TABLE HANDLER redis_tableam_handler;
+")]
+fn redis_tableam_handler(_fcinfo: pg_sys::FunctionCallInfo) -> PgBox<pg_sys::TableAmRoutine> {
+    let mut amroutine =
+        unsafe { PgBox::<pg_sys::TableAmRoutine>::alloc_node(pg_sys::NodeTag::T_TableAmRoutine)};
+
+        // need to implement these for storage engine (or phils eq)
+        amroutine.slot_callbacks = None;
+        amroutine.scan_begin = None;
+        amroutine.scan_getnextslot = None;
+        amroutine.tuple_insert = None;
+        amroutine.relation_set_new_filelocator = None;
+        
+        // gen rest? still does not work
+        amroutine.finish_bulk_insert = None;
+        amroutine.index_build_range_scan = None;
+        amroutine.index_delete_tuples = None;
+        amroutine.index_fetch_begin = None;
+        amroutine.index_fetch_end = None;
+        amroutine.index_fetch_reset = None;
+        amroutine.index_fetch_tuple = None;
+        amroutine.index_validate_scan = None;
+        amroutine.multi_insert = None;
+        amroutine.parallelscan_estimate = None;
+        amroutine.parallelscan_initialize = None;
+        amroutine.parallelscan_reinitialize = None;
+        amroutine.relation_copy_data = None;
+        amroutine.relation_copy_for_cluster = None;
+        amroutine.relation_estimate_size = None;
+        amroutine.relation_fetch_toast_slice = None;
+        amroutine.relation_needs_toast_table = None;
+        amroutine.relation_nontransactional_truncate = None;
+        amroutine.relation_set_new_filelocator = None;
+        amroutine.relation_size = None;
+        amroutine.relation_toast_am = None;
+        amroutine.relation_vacuum = None;
+        amroutine.scan_analyze_next_block = None;
+        amroutine.scan_analyze_next_tuple = None;
+        amroutine.scan_begin = None;
+        amroutine.scan_bitmap_next_block = None;
+        amroutine.scan_bitmap_next_tuple = None;
+        amroutine.scan_end = None;
+        amroutine.scan_getnextslot = None;
+        amroutine.scan_getnextslot_tidrange = None;
+        amroutine.scan_rescan = None;
+        amroutine.scan_sample_next_block = None;
+        amroutine.scan_sample_next_tuple = None;
+        amroutine.scan_set_tidrange = None;
+        amroutine.slot_callbacks = None;
+        amroutine.tuple_complete_speculative = None;
+        amroutine.tuple_delete = None;
+        amroutine.tuple_fetch_row_version = None;
+        amroutine.tuple_get_latest_tid = None;
+        amroutine.tuple_insert = None;
+        amroutine.tuple_insert_speculative = None;
+        amroutine.tuple_lock = None;
+        amroutine.tuple_satisfies_snapshot = None;
+        amroutine.tuple_tid_valid = None;
+        amroutine.tuple_update = None;
+        
+        
 
 
-//         // need to implement these for storage engine (or phils eq)
-//         amroutine.slot_callbacks = None;
-//         amroutine.scan_begin = None;
-//         amroutine.scan_getnextslot = None;
-//         amroutine.tuple_insert = None;
-//         amroutine.relation_set_new_filelocator = None;
+        // also need other functions (like the c implementation just needed stubs for its null checks)
 
-//         // also need other functions (like the c implementation just needed stubs for its null checks)
-
-//     amroutine.into_pg_boxed()
-// }
+    amroutine.into_pg_boxed()
+}
